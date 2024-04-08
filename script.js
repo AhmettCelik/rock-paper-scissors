6987e6b102b2a249ab209407b540947d9d7d7339
AhmettCelik <saimcelikmail@gmail.com>
2024-04-09 01:34:02 +0300

    add some details and round count

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let roundCount = prompt("Enter game round count");
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     options = ["rock", "paper", "scissors"];
     let index = Math.round(Math.random() * 2);
@@ -14,20 +18,7 @@ function checkInput(playerChoice) {
     return continue_break;
 }
 
-function playRound() {
-
-    let playerChoice = getPlayerChoice();
-    let computerChoice = getComputerChoice();
-    
-
-    while(checkInput(playerChoice)) {
-
-        console.log("invalid value! Enter again...");
-        playerChoice = getPlayerChoice();
-
-        checkInput(playerChoice);
-    }
-
+function findWinner(playerChoice, computerChoice) {
     let draw = (playerChoice == "rock" && computerChoice == "rock") || 
                (playerChoice == "paper" && computerChoice == "paper") || 
                (playerChoice == "scissors" && computerChoice == "scissors");
@@ -41,13 +32,51 @@ function playRound() {
                     (playerChoice == "scissors" && computerChoice == "paper");
 
     if(draw) {
-        console.log("Draw");
+        console.log("player chose: " + playerChoice + " computer chose: " + computerChoice);
+        console.log("Draw for this round");
+        computerScore++;
+        playerScore++;
+        console.log("Your score: " + playerScore + " Computer score: " + computerScore);
     }else if(computerWin) {
-        console.log("You Lose");
+        console.log("player chose: " + playerChoice + " computer chose: " + computerChoice);
+        console.log("You lose this round");
+        computerScore++;
+        console.log("Your score: " + playerScore + " Computer score: " + computerScore);
     }else if(playerWin) {
-        console.log("You Win!");
+        console.log("player chose: " + playerChoice + " computer chose: " + computerChoice);
+        console.log("You win this round!");
+        playerScore++;
+        console.log("Your score: " + playerScore + " Computer score: " + computerScore);
     }
+}
+
+function playRound() {
+
+    do {
+
+        var playerChoice = getPlayerChoice();
+        var computerChoice = getComputerChoice();
+        if(checkInput(playerChoice)) console.log("invalid value! Enter again...");
+
+        findWinner(playerChoice, computerChoice);
+
+    }while(checkInput(playerChoice));
 
 }
 
-playRound();
+function playGame(roundCount) {
+    for(let i = 0; i < roundCount; i++) {
+        playRound();
+        if((i == roundCount - 1) && (computerScore == playerScore)) {
+            console.log("Extra round!!");
+            roundCount++;
+        }
+    }
+    if(computerScore > playerScore) {
+        console.log("You Lose!")
+    }else if(playerScore > computerScore) {
+        console.log("You Win!");
+    }
+}
+
+playGame(roundCount);
